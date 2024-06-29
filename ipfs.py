7b270b90dedc5c6d14dcfd469524cad4077200a5
yta1116 <157062785+yta1116@users.noreply.github.com>
2024-06-29 16:25:07 -0400

    Update ipfs.py

--- ipfs.py
+++ ipfs.py
@@ -4,21 +4,21 @@ import json
 def pin_to_ipfs(data):
 	assert isinstance(data,dict), f"Error pin_to_ipfs expects a dictionary"
 	#YOUR CODE HERE
-	api_key = "73feb8f6272c73bf972a"
-	api_secret = "a6c1890d357fcec346491c20f439bcd292ee896155ea41151ba0610936a38c62"
+	#api_key = "73feb8f6272c73bf972a"
+	#api_secret = "a6c1890d357fcec346491c20f439bcd292ee896155ea41151ba0610936a38c62"
 	
 	# Convert the dictionary to JSON
 	json_data = json.dumps(data)
 
 	# Define the headers and URL for Pinata API
 	headers = {
-		"Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySW5mb3JtYXRpb24iOnsiaWQiOiJkMDcyMDBiYi04ODU0LTQwZmUtOTVlZS0wOWVkOWUxZTUxYzYiLCJlbWFpbCI6Inl1bmZlaXRhQGJlcmtlbGV5LmVkdSIsImVtYWlsX3ZlcmlmaWVkIjp0cnVlLCJwaW5fcG9saWN5Ijp7InJlZ2lvbnMiOlt7ImRlc2lyZWRSZXBsaWNhdGlvbkNvdW50IjoxLCJpZCI6IkZSQTEifSx7ImRlc2lyZWRSZXBsaWNhdGlvbkNvdW50IjoxLCJpZCI6Ik5ZQzEifV0sInZlcnNpb24iOjF9LCJtZmFfZW5hYmxlZCI6ZmFsc2UsInN0YXR1cyI6IkFDVElWRSJ9LCJhdXRoZW50aWNhdGlvblR5cGUiOiJzY29wZWRLZXkiLCJzY29wZWRLZXlLZXkiOiI3M2ZlYjhmNjI3MmM3M2JmOTcyYSIsInNjb3BlZEtleVNlY3JldCI6ImE2YzE4OTBkMzU3ZmNlYzM0NjQ5MWMyMGY0MzliY2QyOTJlZTg5NjE1NWVhNDExNTFiYTA2MTA5MzZhMzhjNjIiLCJleHAiOjE3NTEyMjgxMTl9.w3EYpH4Ep4YH4HjKGUn1dbi_hPK-bA_FWWYAsq6v5KA",
+		"Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySW5mb3JtYXRpb24iOnsiaWQiOiJkMDcyMDBiYi04ODU0LTQwZmUtOTVlZS0wOWVkOWUxZTUxYzYiLCJlbWFpbCI6Inl1bmZlaXRhQGJlcmtlbGV5LmVkdSIsImVtYWlsX3ZlcmlmaWVkIjp0cnVlLCJwaW5fcG9saWN5Ijp7InJlZ2lvbnMiOlt7ImRlc2lyZWRSZXBsaWNhdGlvbkNvdW50IjoxLCJpZCI6IkZSQTEifSx7ImRlc2lyZWRSZXBsaWNhdGlvbkNvdW50IjoxLCJpZCI6Ik5ZQzEifV0sInZlcnNpb24iOjF9LCJtZmFfZW5hYmxlZCI6ZmFsc2UsInN0YXR1cyI6IkFDVElWRSJ9LCJhdXRoZW50aWNhdGlvblR5cGUiOiJzY29wZWRLZXkiLCJzY29wZWRLZXlLZXkiOiJjZDUxZDA1ZmMxZTM4ZDVhYTRhNSIsInNjb3BlZEtleVNlY3JldCI6ImEzZDVjMTlkYTA1MzQ2ZmFhYTllM2VjOWQyMTBjNzRlZmU5ZDM3MWE5MDgxZmZkN2ZkZDE2MTVlN2RlZmE2ZDkiLCJleHAiOjE3NTEyMjg2MjZ9.gm_4cz2VXiY7KGXBxTyqRLbOEstoEgfsfH0vn-ZcP1M",
 		"Content-Type": "application/json"
 	}
 	url = "https://api.pinata.cloud/pinning/pinJSONToIPFS"
     
 	# Send the request to Pinata
-	response = requests.request("POST", url, json=json_data, headers=headers, auth=(api_key, api_secret))
+	response = requests.request("POST", url, json=json_data, headers=headers)
     
 	# Check if the request was successful
 	if response.status_code == 200:
