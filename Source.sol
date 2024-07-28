2a543f074ddf204b9235d6a8fa41b1ad3d21a222
yta1116 <157062785+yta1116@users.noreply.github.com>
2024-07-28 17:26:59 -0400

    Add files via upload

---
+++ Source.sol
@@ -0,0 +1,57 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
+
+contract Source is AccessControl {
+    bytes32 public constant ADMIN_ROLE = keccak256("ADMIN_ROLE");
+    bytes32 public constant WARDEN_ROLE = keccak256("BRIDGE_WARDEN_ROLE");
+	mapping( address => bool) public approved;
+	address[] public tokens;
+
+	event Deposit( address indexed token, address indexed recipient, uint256 amount );
+	event Withdrawal( address indexed token, address indexed recipient, uint256 amount );
+	event Registration( address indexed token );
+
+    constructor( address admin ) {
+        _grantRole(DEFAULT_ADMIN_ROLE, admin);
+        _grantRole(ADMIN_ROLE, admin);
+        _grantRole(WARDEN_ROLE, admin);
+
+    }
+
+	function deposit(address _token, address _recipient, uint256 _amount ) public {
+		//YOUR CODE HERE
+		require(approved[_token], "Token not registered");
+    require(_amount > 0, "Amount must be greater than zero");
+
+    ERC20(_token).transferFrom(msg.sender, address(this), _amount);
+
+    emit Deposit(_token, _recipient, _amount);
+	}
+
+	function withdraw(address _token, address _recipient, uint256 _amount ) onlyRole(WARDEN_ROLE) public {
+		//YOUR CODE HERE
+		require(_amount > 0, "Amount must be greater than zero");
+
+    ERC20(_token).transfer(_recipient, _amount);
+
+    emit Withdrawal(_token, _recipient, _amount);
+	}
+
+	function registerToken(address _token) onlyRole(ADMIN_ROLE) public {
+		//YOUR CODE HERE
+		require(_token != address(0), "Token address cannot be zero");
+    require(!approved[_token], "Token already registered");
+
+    approved[_token] = true;
+    tokens.push(_token);
+
+    emit Registration(_token);
+	}
+
+
+}
+
+
